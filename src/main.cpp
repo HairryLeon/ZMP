e45ec2f4413653bcdd032b68cde5361fec9694e0
Szymon Omieljaniuk <275422@student.pwr.edu.pl>
2025-11-25 23:52:15 +0100

    Łączenie z serwerem działa, ale nic nie rysuje

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,38 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <sstream>
 #include <memory>
 #include <xercesc/util/PlatformUtils.hpp>
 #include <xercesc/sax2/SAX2XMLReader.hpp>
 #include <xercesc/sax2/XMLReaderFactory.hpp>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
 #include "xmlinterp.hh"
 #include "Configuration.hh"
 #include "RunPreprocessor.hh"
 #include "CommandRegistry.hh"
 #include "RunProgram.hh"
+#include "Port.hh"
+#include "AccessControl.hh"
+
+int Send(int Sk2Server, const char *sMesg)
+{
+	ssize_t IlWyslanych;
+	ssize_t IlDoWyslania = (ssize_t)strlen(sMesg);
+	while((IlWyslanych = write(Sk2Server, sMesg, IlDoWyslania)) > 0 ){
+		IlDoWyslania -=IlWyslanych;
+		sMesg += IlWyslanych;
+	}
+	if(IlWyslanych < 0) {
+		std::cerr << "*** Blad przeslania napisu." << std::endl;
+	}
+	return 0;
+}
 
 int main()
 {
@@ -38,6 +61,42 @@ int main()
 		for(const auto &lib : cfg.GetLibs()){
 			registry.LoadAndRegister(std::string("./libs/") + lib);
 		}
+
+		int sock = socket(AF_INET, SOCK_STREAM, 0);
+		sockaddr_in sa{};
+		sa.sin_family = AF_INET;
+		sa.sin_port = htons(PORT);
+		inet_pton(AF_INET, "127.0.0.1", &sa.sin_addr);
+
+		if (connect(sock, (sockaddr*)&sa, sizeof(sa)) != 0) {
+    			perror("connect");
+    			return 1;
+		}
+
+
+		Send(sock, "Clear\n");
+
+
+		auto cubes = cfg.GetCubes();
+		std::vector<Configuration::Cube> sorted = cubes;   // kopia, żeby móc sortować
+		std::sort(sorted.begin(), sorted.end(),
+          	[](const auto& a, const auto& b){ return a.Name.size() < b.Name.size(); });
+
+
+		for (const auto &c : sorted) {
+    			std::ostringstream cmd;
+    			cmd << "AddObj Name=" << c.Name
+        		<< " Shift=("     << c.Shift[0]     << "," << c.Shift[1]     << "," << c.Shift[2]     << ")"
+        		<< " Scale=("     << c.Scale[0]     << "," << c.Scale[1]     << "," << c.Scale[2]     << ")"
+        		<< " RotXYZ_deg=("<< c.RotXYZ_deg[0]<< "," << c.RotXYZ_deg[1]<< "," << c.RotXYZ_deg[2]<< ")"
+        		<< " Trans_m=("   << c.Trans_m[0]   << "," << c.Trans_m[1]   << "," << c.Trans_m[2]   << ")"
+        		<< " RGB=("       << c.RGB[0]       << "," << c.RGB[1]       << "," << c.RGB[2]       << ")"
+        		<< "\n";
+    			Send(sock, cmd.str().c_str());
+		}
+
+		Send(sock, "Close\n");
+		close(sock);
 	}
 	xercesc::XMLPlatformUtils::Terminate();
 
